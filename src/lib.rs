c88becec3a2021d1c3d7b128165be4ddb252d934
Navneet Aman <contact@navneetaman.com>
2026-06-15 07:52:24 +0530

    parser facade

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod command;
+mod parser;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
---
+++ src/parser.rs
@@ -0,0 +1,5 @@
+use crate::command::Command;
+
+pub fn parser(input: &str) -> Command {
+    todo!()
+}

8674f2f658f51659ab7f51156b0f38ca3f8cde27
Navneet Aman <contact@navneetaman.com>
2026-06-15 13:52:23 +0530

    add test for contains_str

--- src/ext.rs
+++ src/ext.rs
@@ -6,3 +6,19 @@ impl ContainsStr for Vec<String> {
         self.into_iter().any(|s| s == input)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ext::ContainsStr;
+
+    #[test]
+    fn t_contains() {
+        let words = vec!["apple".to_string(), "banana".to_string(), "mango".to_string()];
+        assert!(words.contains_str("apple"));
+    }
+    #[test]
+    fn t_not_contains() {
+        let words = vec!["apple".to_string(), "banana".to_string(), "mango".to_string()];
+        assert!(!words.contains_str("dog"));
+    }
+}

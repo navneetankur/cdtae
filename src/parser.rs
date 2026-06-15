e8e257a2f2f2f01d0bc84c5519c0f522a6768496
Navneet Aman <contact@navneetaman.com>
2026-06-15 08:36:28 +0530

    test for redo and replace.

--- src/parser.rs
+++ src/parser.rs
@@ -14,10 +14,22 @@ mod tests {
     use crate::command;
 
 use super::*;
-
     #[test]
     fn t_parse_undo() {
         let output = parse("undo");
         assert_eq!(output, Ok(Command::Undo));
+    }
+    #[test]
+    fn t_parse_redo() {
+        let output = parse("redo");
+        assert_eq!(output, Ok(Command::Redo));
+    }
+    #[test]
+    fn t_parse_replace() {
+        let output = parse("replace x with y");
+        assert_eq!(output, Ok(Command::Replace(command::Replace{ 
+            from: "x",
+            to: "y",
+        })));
     }
 }

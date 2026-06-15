9b4d94e48e592159a98860ae603c1ddfdd108710
Navneet Aman <contact@navneetaman.com>
2026-06-15 08:26:51 +0530

    add undo parse test

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,23 @@
 use crate::command::Command;
+#[derive(thiserror::Error, Debug, PartialEq)]
+pub enum ParseError {
+    #[error("malformed replace")]
+    MalformedReplace,
+}
+type ParseResult<T> = Result<T, ParseError>;
 
-pub fn parser(input: &str) -> Command {
+pub fn parse(input: &str) -> ParseResult<Command> {
     todo!()
+}
+#[cfg(test)]
+mod tests {
+    use crate::command;
+
+use super::*;
+
+    #[test]
+    fn t_parse_undo() {
+        let output = parse("undo");
+        assert_eq!(output, Ok(Command::Undo));
+    }
 }
